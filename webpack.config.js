4995c58fe2642320565a7f0c064f193ff0fe908e
nickbreaton <nick@breaton.com>
2017-03-06 14:18:22 -0500

    Clean up

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,17 @@
 const { resolve } = require('path');
 
 module.exports = {
-  entry: resolve(__dirname, 'src/index.js'),
+  entry: resolve(__dirname, 'examples/index.js'),
   output: {
     publicPath: 'dist/',
     filename: 'index.js'
   },
   module: {
     rules: [
-      { test: /\/vanilla\/.*\.css$/,  use: ['style-loader', 'css-loader'] },
-      { test: /\/css-modules\/.*\.css$/,  use: ['style-loader', 'css-loader?modules'] },
-      { test: /\.js$/,   use: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.json$/, use: 'json-loader' },
+      { test: /\/vanilla\/.*\.css$/, use: ['style-loader', 'css-loader'] },
+      { test: /\/css-modules\/.*\.css$/, use: ['style-loader', 'css-loader?modules'] },
+      { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.json$/, use: 'json-loader' }
     ]
   }
 }
